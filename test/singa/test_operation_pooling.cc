cb4445af95633c1a22dc7b1c78301cf1d0fd02cc
dcslin <13751447+dcslin@users.noreply.github.com>
2018-12-24 14:28:41 +0800

    runnable pooling backward

--- test/singa/test_operation_pooling.cc
+++ test/singa/test_operation_pooling.cc
@@ -25,6 +25,7 @@
 
 //using singa::Pooling;
 //using singa::Shape;
+using namespace singa;
 
 TEST(OperationPooling, Forward) {
   const size_t batchsize = 2, c = 1, h = 3, w = 3;
@@ -99,16 +100,16 @@ TEST(OperationPooling, Forward) {
 
 }
 
-//
-//TEST(Pooling, Backward) {
-//  // src_data
-//  const size_t batchsize = 2, c = 1, src_h = 3, src_w = 3;
-//  const float x[batchsize * c * src_h * src_w] = {
-//      1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f,
-//      1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
-//  singa::Tensor in(singa::Shape{batchsize, c, src_h, src_w});
-//  in.CopyDataFromHostPtr(x, batchsize * c * src_h * src_w);
-//
+
+TEST(OperationPooling, Backward) {
+  // src_data
+  const size_t batchsize = 2, c = 1, src_h = 3, src_w = 3;
+  const float x[batchsize * c * src_h * src_w] = {
+      1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f,
+      1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f};
+  singa::Tensor in(singa::Shape{batchsize, c, src_h, src_w});
+  in.CopyDataFromHostPtr(x, batchsize * c * src_h * src_w);
+
 //  Pooling pool;
 //  singa::LayerConf conf;
 //  singa::PoolingConf *poolconf = conf.mutable_pooling_conf();
@@ -120,37 +121,121 @@ TEST(OperationPooling, Forward) {
 //  poolconf->set_stride_h(1);
 //  poolconf->set_stride_w(1);
 //  pool.Setup(Shape{1, 3, 3}, conf);
-//
+
 //  singa::Tensor out1 = pool.Forward(singa::kTrain, in);
-//
-//  // grad
-//  const size_t grad_h = 2, grad_w = 2;
-//  const float dy[batchsize * c * grad_h * grad_w] = {0.1f, 0.2f, 0.3f, 0.4f,
-//                                                     0.1f, 0.2f, 0.3f, 0.4f};
-//  singa::Tensor grad(singa::Shape{batchsize, c, grad_h, grad_w});
-//  grad.CopyDataFromHostPtr(dy, batchsize * c * grad_h * grad_w);
-//
+
+
+  singa::Tensor y(Shape{2,1,2,2});
+
+
+  try {
+    using namespace mkldnn;
+
+    mkldnn::engine eng = mkldnn::engine(mkldnn::engine::cpu, 0);
+
+    std::vector<primitive> net;
+    memory::dims x_dims={batchsize,c,3,3};
+    memory::dims y_dims={batchsize,c,2,2};
+    memory::dims s_dims={1,1};
+    memory::dims k_dims={2,2};
+    memory::dims p_dims={0,0};
+    auto x_md= memory::desc({x_dims},memory::data_type::f32, memory::format::nchw);
+    auto y_md= memory::desc({y_dims},memory::data_type::f32, memory::format::nchw);
+
+//  auto pool_d=pooling_forward::desc(prop_kind::forward_inference, pooling_max, x_md, y_md, s_dims, k_dims, p_dims, p_dims, padding_kind::zero);
+// inference will ignore ws
+//    auto pool_fwd_d=pooling_forward::desc(forward_inference, pooling_max, x_md, y_md, s_dims, k_dims, p_dims, p_dims, padding_kind::zero);
+
+
+// training will require ws
+    auto pool_fwd_d=pooling_forward::desc(forward_training, pooling_max, x_md, y_md, s_dims, k_dims, p_dims, p_dims, padding_kind::zero);
+    auto pool_fwd_pd = pooling_forward::primitive_desc(pool_fwd_d, eng);
+
+    auto y_mem = memory(pool_fwd_pd.dst_primitive_desc());
+    auto x_mem = memory({{{x_dims}, mkldnn::memory::data_type::f32, mkldnn::memory::format::nchw}, eng}, in.block()->mutable_data());
+
+    y_mem.set_data_handle(y.block()->mutable_data());
+
+    auto pool_ws_d= pool_fwd_pd.workspace_primitive_desc();
+    auto ws_mem = memory(pool_ws_d);
+
+    net.push_back( pooling_forward(pool_fwd_pd, x_mem, y_mem, ws_mem));
+    stream(stream::kind::eager).submit(net).wait();
+
+    // grad - bwd
+    const size_t grad_h = 2, grad_w = 2;
+    const float dy[batchsize * c * grad_h * grad_w] = {0.1f, 0.2f, 0.3f, 0.4f,
+                                                       0.1f, 0.2f, 0.3f, 0.4f};
+    singa::Tensor grad(singa::Shape{batchsize, c, grad_h, grad_w});
+    grad.CopyDataFromHostPtr(dy, batchsize * c * grad_h * grad_w);
+
+
+    singa::Tensor in_grad(Shape{2,1,3,3});
+
+    auto pool_bwd_d=pooling_backward::desc( pooling_max, x_md, y_md, s_dims, k_dims, p_dims, p_dims, padding_kind::zero);
+
+    auto pool_bwd_pd = pooling_backward::primitive_desc( pool_bwd_d, eng, pool_fwd_pd);
+
+    auto dx_mem = memory({{{x_dims}, mkldnn::memory::data_type::f32, mkldnn::memory::format::nchw}, eng}, in_grad.block()->mutable_data());
+    auto dy_mem = memory({{{2,1,2,2}, mkldnn::memory::data_type::f32, mkldnn::memory::format::nchw}, eng}, grad.block()->mutable_data());
+
+    net.push_back( pooling_backward(pool_bwd_pd, dy_mem, ws_mem, dx_mem) );
+    stream(stream::kind::eager).submit(net).wait();
+
+
+    const float *dx = in_grad.data<float>();
+    EXPECT_EQ(18u, in_grad.Size());
+    EXPECT_EQ(0.0f, dx[0]);
+    EXPECT_EQ(0.0f, dx[1]);
+    EXPECT_EQ(0.0f, dx[2]);
+    EXPECT_EQ(0.0f, dx[3]);
+    EXPECT_EQ(0.1f, dx[4]);
+    EXPECT_EQ(0.2f, dx[5]);
+    EXPECT_EQ(0.0f, dx[6]);
+    EXPECT_EQ(0.3f, dx[7]);
+    EXPECT_EQ(0.4f, dx[8]);
+    EXPECT_EQ(0.0f, dx[9]);
+    EXPECT_EQ(0.0f, dx[10]);
+    EXPECT_EQ(0.0f, dx[11]);
+    EXPECT_EQ(0.0f, dx[12]);
+    EXPECT_EQ(0.1f, dx[13]);
+    EXPECT_EQ(0.2f, dx[14]);
+    EXPECT_EQ(0.0f, dx[15]);
+    EXPECT_EQ(0.3f, dx[16]);
+    EXPECT_EQ(0.4f, dx[17]);
+    /*
+     */
+
+  }
+  catch (mkldnn::error &e) {
+    LOG(FATAL) << "MKLDNN pooling bwd" << "Status: " << e.status << " Message: " << e.message;
+  }
+/*
+
 //  const auto ret = pool.Backward(singa::kTrain, grad);
-//  singa::Tensor in_grad = ret.first;
-//  const float *dx = in_grad.data<float>();
-//  EXPECT_EQ(18u, in_grad.Size());
-//  EXPECT_EQ(0.0f, dx[0]);
-//  EXPECT_EQ(0.0f, dx[1]);
-//  EXPECT_EQ(0.0f, dx[2]);
-//  EXPECT_EQ(0.0f, dx[3]);
-//  EXPECT_EQ(0.1f, dx[4]);
-//  EXPECT_EQ(0.2f, dx[5]);
-//  EXPECT_EQ(0.0f, dx[6]);
-//  EXPECT_EQ(0.3f, dx[7]);
-//  EXPECT_EQ(0.4f, dx[8]);
-//  EXPECT_EQ(0.0f, dx[9]);
-//  EXPECT_EQ(0.0f, dx[10]);
-//  EXPECT_EQ(0.0f, dx[11]);
-//  EXPECT_EQ(0.0f, dx[12]);
-//  EXPECT_EQ(0.1f, dx[13]);
-//  EXPECT_EQ(0.2f, dx[14]);
-//  EXPECT_EQ(0.0f, dx[15]);
-//  EXPECT_EQ(0.3f, dx[16]);
-//  EXPECT_EQ(0.4f, dx[17]);
-//}
+
+
+  const float *dx = in_grad.data<float>();
+  EXPECT_EQ(18u, in_grad.Size());
+  EXPECT_EQ(0.0f, dx[0]);
+  EXPECT_EQ(0.0f, dx[1]);
+  EXPECT_EQ(0.0f, dx[2]);
+  EXPECT_EQ(0.0f, dx[3]);
+  EXPECT_EQ(0.1f, dx[4]);
+  EXPECT_EQ(0.2f, dx[5]);
+  EXPECT_EQ(0.0f, dx[6]);
+  EXPECT_EQ(0.3f, dx[7]);
+  EXPECT_EQ(0.4f, dx[8]);
+  EXPECT_EQ(0.0f, dx[9]);
+  EXPECT_EQ(0.0f, dx[10]);
+  EXPECT_EQ(0.0f, dx[11]);
+  EXPECT_EQ(0.0f, dx[12]);
+  EXPECT_EQ(0.1f, dx[13]);
+  EXPECT_EQ(0.2f, dx[14]);
+  EXPECT_EQ(0.0f, dx[15]);
+  EXPECT_EQ(0.3f, dx[16]);
+  EXPECT_EQ(0.4f, dx[17]);
+
+  */
+}
 
