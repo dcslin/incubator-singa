a9fb505315951f07735c1bd4fef1df27df90d51b
dcslin <13751447+dcslin@users.noreply.github.com>
2020-03-04 08:39:55 +0000

    test1

--- tool/travis/build.sh
+++ tool/travis/build.sh
@@ -29,6 +29,40 @@ suffix=$TRAVIS_JOB_NUMBER  #`TZ=Asia/Singapore date +%Y-%m-%d-%H-%M-%S`
 export CONDA_BLD_PATH=~/conda-bld-$suffix
 mkdir $CONDA_BLD_PATH
 
+### DELETEME
+TRAVIS_SECURE_ENV_VARS="true"
+RELEASE_TYPE="patch"
+### DELETEME
+
+if [[ "$TRAVIS_SECURE_ENV_VARS" != "false" ]]
+then
+  VERSION=`git describe --abbrev=0 --tags`
+
+  VERSION_BITS=(${VERSION//./ })
+
+  VNUM1=${VERSION_BITS[0]}
+  VNUM2=${VERSION_BITS[1]}
+  VNUM3=${VERSION_BITS[2]}
+
+  if [[ "$RELEASE_TYPE" == "major" ]]; then
+    VNUM1=$((VNUM1+1))
+  elif [[ "$RELEASE_TYPE" == "minor" ]]; then
+    VNUM2=$((VNUM2+1))
+  elif [[ "$RELEASE_TYPE" == "patch" ]]; then
+    VNUM3=$((VNUM3+1))
+  else
+    echo "Release type is one of [major|minor|patch]"
+    exit 1
+  fi
+
+  NEW_VERSION="$VNUM1.$VNUM2.$VNUM3"
+  echo "Updating $VERSION to $NEW_VERSION"
+  # github access
+  # git tag $NEW_VERSION
+  # git push --tags
+  # echo "Tag created and pushed to github: $NEW_VERSION"
+fi
+
 conda build tool/conda/singa --python 3.6
 conda build tool/conda/singa --python 3.7
 # conda install --use-local singa
@@ -43,5 +77,8 @@ else
   # turn off debug to hide the token in travis log
   set +x
   # upload the package onto anaconda cloud
+
+  ANACONDA_UPLOAD_TOKEN="sh-c76ce9b7-676b-402b-ac05-32e140ae5221"
+  USER="shicong"
   anaconda -t $ANACONDA_UPLOAD_TOKEN upload -u $USER -l main $CONDA_BLD_PATH/$OS/singa-*.tar.bz2 --force
 fi
